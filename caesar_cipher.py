e4401cea7a083ceb7392e261e9dd024045f32405
Ekaterina <pirit544@gmail.com>
2020-09-20 14:39:35 +0500

    add function for decryption

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,10 +3,26 @@ def encrypt(text, shift):
 
     for i in range(len(text)):
         char = text[i]
-        if (char.isupper()):
+        if char == ' ':
+            result += char
+        elif (char.isupper()):
             result += chr((ord(char) + shift - 65) % 26 + 65)
         else:
             result += chr((ord(char) + shift - 97) % 26 + 97)
 
     return result
 
+
+def decrypt(text, shift):
+    result = ''
+
+    for i in range(len(text)):
+        char = text[i]
+        if char == ' ':
+            result += char
+        elif (char.isupper()):
+            result += chr((ord(char) - shift + 65) % 26 + 65)
+        else:
+            result += chr((ord(char) - shift + 85) % 26 + 97)
+
+    return result
