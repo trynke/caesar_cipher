e44e58d374c9b2f8d700f9ca87e580e1248dcdf4
Ekaterina <pirit544@gmail.com>
2020-10-25 20:04:50 +0500

    add logging

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -37,29 +37,50 @@ def decrypt(text, shift):
 
 # Main logic of the program
 def main():
+    format = "%(asctime)s - %(message)s"
+    logging.basicConfig(filename="cipher.log", level=logging.INFO, format=format)
+
     state = True
     print("Hello there! \n")
+    logging.info('Program started')
 
     while state:
         # Mode selection
         mode = str(input("Type an integer to choose a mode. \n1. Encrypt text \n2. Decrypt text \n3. Exit \n"))
+        logging.info(f'User chose mode {mode}')
 
         if mode == '1':
             text = str(input("Enter some text (only latin alphabet): "))
-            shift = int(input("Enter a cipher shift: "))
-            print(f'Encrypted text: {encrypt(text, shift)} \n')
+            logging.info(f'User entered text: {text}')
+            try:
+                shift = int(input("Enter a cipher shift: "))
+                logging.info(f"User entered shift {shift}")
+                print(f'Encrypted text: {encrypt(text, shift)} \n')
+                logging.info(f'The encrypted text: {encrypt(text, shift)}')
+            except ValueError:
+                print("It is not an integer. Let's try again. \n")
+                logging.info(f'User entered wrong shift value')
 
         elif mode == '2':
             text = str(input("Enter encrypted text (only latin alphabet): "))
-            shift = int(input("Enter a cipher shift: "))
-            print(f'Decrypted text: {decrypt(text, shift)} \n')
+            logging.info(f'User entered text: {text}')
+            try:
+                shift = int(input("Enter a cipher shift: "))
+                logging.info(f"User entered shift {shift}")
+                print(f'Decrypted text: {decrypt(text, shift)} \n')
+                logging.info(f'The decrypted text: {decrypt(text, shift)}')
+            except ValueError:
+                print("It is not an integer. Let's try again. \n")
+                logging.info(f'User entered wrong shift value')
 
         elif mode == '3':
             print('Goodbye!')
+            logging.info("Program ended")
             state = False
 
         else:
             # Error handling
+            logging.info("User entered incorrect value")
             print("Incorrect input. Please, try again \n")
 
 
